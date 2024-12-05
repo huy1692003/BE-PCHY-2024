5bfc79c1510ca3a83a4ab91a5257faea7229c8e4
VinhVu2003 <vuv28104@gmail.com>
2024-12-05 16:12:50 +0700

    vinh

---
+++ API_PCHY/Models/QLTN/QLTN_THIET_BI_YCTN/QLTN_THIET_BI_Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace API_PCHY.Models.QLTN.QLTN_THIET_BI_YCTN
+{
+    public class QLTN_THIET_BI_Model
+    {
+        public string id { get; set; }
+        public string? ma_tbtn { get; set; }
+        public string? ma_yctn { get; set; }
+        public string? ten_thiet_bi { get; set; }
+        public string? ma_loai_tb { get; set; }
+        public int? so_luong { get; set; }
+
+        public int? trang_thai { get; set; }
+
+        public DateTime? ngay_tao { get; set; }
+
+        public string? nguoi_tao { get; set; }
+        public string? ten_loai_thiet_bi { get; set; }
+
+
+    }
+}
